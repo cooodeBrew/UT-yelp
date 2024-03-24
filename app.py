7fa72643e10cb257da493bf60dd643ce90a62781
anjieliu121 <123579334+anjieliu121@users.noreply.github.com>
2024-03-23 19:13:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 import streamlit as st
 import json
-from propelauth import auth
-
-user = auth.get_user()
-if user is None:
-    st.error('Unauthorized')
-    st.stop()
-
-with st.sidebar:
-    st.link_button('Account', auth.get_account_url(), use_container_width=True)
-
-st.text("Logged in as " + user.email + " with user ID " + user.user_id)
 with open('./data/03232024.json') as file:
     data = json.load(file)
 
